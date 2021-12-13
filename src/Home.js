74d4d7922be5de86d83f09e10ceb8a3d94469dec
Segun <adeniyisegun025@gmail.com>
2021-12-13 19:18:31 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import react from 'react'
+import reactDom from 'react-dom'
+
+
+function Home() {
+return(
+
+    <div>
+
+        <p>This is react! </p>
+    </div>
+    )
+}
+
+
+
+
+
+
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './Home';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    {/* <Home />,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode> */}
+    <Home />
+  </div>,
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
